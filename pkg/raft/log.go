192da8ef721d78643d0ef97aaae2384aaf3f3f65
Rounak Patra <rounakpatra2000@gmail.com>
2022-02-08 20:47:01 +0530

    log structs updated

--- pkg/raft/log.go
+++ pkg/raft/log.go
@@ -1,11 +1,37 @@
 package raft
 
-// 4 types of msg : VoteRequest, VoteResponse, LogRequest, LogResponse
-
-type logEntry struct {
+// PLEASE DO SOMETHING ABOUT MSG
+type Msg struct {
 }
 
-type log struct {
-	entry logEntry
-	next  *logEntry
+type LogEntry struct {
+	m    Msg
+	term int
+	next *LogEntry
+}
+
+type Log struct {
+	head   *LogEntry
+	length int
+}
+
+func (l *Log) initLog() {
+	l.head = nil
+	l.length = 0
+}
+
+func (l *Log) append(e *LogEntry) {
+	if l.length == 0 {
+		l.head = e
+		l.length += 1
+		return
+	}
+
+	temp := l.head
+	for i := 0; i < l.length-1; i++ {
+		temp = temp.next
+	}
+	temp.next = e
+	l.length += 1
+
 }
