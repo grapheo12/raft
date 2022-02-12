4585e1f1ee5a8dee559f8ec86ef9e4eff201bee2
Shubham Mishra <grapheo12@gmail.com>
2022-02-12 16:59:21 +0530

    Handle Follower

--- pkg/raft/log.go
+++ pkg/raft/log.go
@@ -5,23 +5,50 @@ import (
 )
 
 type LogType struct {
-	LogArray     []rpc.LogEntry
+	LogArray     []*rpc.LogEntry
 	Length       int
 	CommitLength int
 	LastTerm     int32
 }
 
 func (l *LogType) Init() {
-	l.LogArray = []rpc.LogEntry{}
+	l.LogArray = []*rpc.LogEntry{}
 	l.Length = 0
 	l.CommitLength = 0
 	l.LastTerm = 0
 }
 
-func (l *LogType) Append(e rpc.LogEntry) {
+func (l *LogType) Append(e *rpc.LogEntry) {
 	l.LogArray = append(l.LogArray, e)
 	l.Length++
 	if e.Term > l.LastTerm {
 		l.LastTerm = e.Term
+	}
+}
+
+func (l *LogType) AppendEntries(prefixLen int32, leaderCommitLen int32, suffix []*rpc.LogEntry) {
+	if len(suffix) > 0 && l.Length > int(prefixLen) {
+		index := l.Length
+		if index > int(prefixLen)+len(suffix) {
+			index = int(prefixLen) + len(suffix)
+		}
+		index--
+
+		if l.LogArray[index].Term != suffix[index-int(prefixLen)].Term {
+			l.LogArray = l.LogArray[:prefixLen]
+			l.Length = int(prefixLen)
+			l.LastTerm = l.LogArray[prefixLen-1].Term
+		}
+	}
+
+	if int(prefixLen)+len(suffix) > l.Length {
+		l.LogArray = append(l.LogArray, suffix[l.Length-int(prefixLen):]...)
+		l.Length = len(l.LogArray)
+		l.LastTerm = l.LogArray[l.Length-1].Term
+	}
+
+	if leaderCommitLen > int32(l.CommitLength) {
+		// TODO: DELIVER
+		l.CommitLength = int(leaderCommitLen)
 	}
 }
--- pkg/raft/node.go
+++ pkg/raft/node.go
@@ -1,7 +1,11 @@
 package raft
 
 import (
+	"context"
+	"errors"
 	"math"
+	"math/rand"
+	"raft/internal/lo"
 	"raft/pkg/network"
 	"raft/pkg/rpc"
 	"time"
@@ -16,13 +20,17 @@ const (
 )
 
 type RaftNode struct {
-	n           *network.Network
-	nId         int32
-	normalQId   int32
-	electionQId int32
+	n               *network.Network
+	nId             int32
+	voteRequestQId  int32
+	voteResponseQId int32
+	logRequestQId   int32
+	logResponseQId  int32
 
-	normalCh   network.Queue
-	electionCh network.Queue
+	voteRequestCh  network.Queue
+	voteResponseCh network.Queue
+	logRequestCh   network.Queue
+	logResponseCh  network.Queue
 
 	State int   // FOLLOWER, CANDIDATE, LEADER
 	Term  int32 // Current Election Term
@@ -38,19 +46,27 @@ type RaftNode struct {
 }
 
 func (r *RaftNode) Init(
-	n *network.Network, normalQId, electionQId int32,
+	n *network.Network,
+	voteRequestQId, voteResponseQId int32,
+	logRequestQId, logResponseQId int32,
 	eMinT, eMaxT, cT time.Duration) {
 
 	r.n = n
 	r.nId = n.NodeId
-	r.normalQId = normalQId
-	r.electionQId = electionQId
+	r.voteRequestQId = voteRequestQId
+	r.voteResponseQId = voteResponseQId
+	r.logRequestQId = logRequestQId
+	r.logResponseQId = logResponseQId
 
-	r.normalCh = make(network.Queue)
-	r.electionCh = make(network.Queue)
+	r.voteRequestCh = make(network.Queue)
+	r.voteResponseCh = make(network.Queue)
+	r.logRequestCh = make(network.Queue)
+	r.logResponseCh = make(network.Queue)
 
-	r.n.RegisterQueue(r.normalQId, r.normalCh)
-	r.n.RegisterQueue(r.electionQId, r.electionCh)
+	r.n.RegisterQueue(r.logRequestQId, r.logRequestCh)
+	r.n.RegisterQueue(r.logResponseQId, r.logResponseCh)
+	r.n.RegisterQueue(r.voteRequestQId, r.voteRequestCh)
+	r.n.RegisterQueue(r.voteResponseQId, r.voteResponseCh)
 
 	r.electionMaxTimeout = eMaxT
 	r.electionMinTimeout = eMinT
@@ -64,6 +80,246 @@ func (r *RaftNode) Init(
 	r.CurrLeaderId = -1 // no leader now
 	r.VotedFor = r.nId
 	r.VotesReceived = make(map[int32]bool)
+}
+
+// func (r *RaftNode) NodeMain(ctx context.Context) {
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			return
+// 		case data := <-r.voteRequestCh:
+// 			voteReq := rpc.VoteRequestMsg{}
+// 			err := voteReq.Unmarshal(data.Data)
+// 			if err != nil {
+// 				lo.RaftError(r.nId, err.Error(), data)
+// 			}
+
+// 			if r.State == FOLLOWER {
+
+// 			} else if r.State == CANDIDATE {
+
+// 			} else { // LEADER
+
+// 			}
+
+// 		case data := <-r.voteResponseCh:
+// 			voteResp := rpc.VoteResponseMsg{}
+// 			err := voteResp.Unmarshal(data.Data)
+// 			if err != nil {
+// 				lo.RaftError(r.nId, err.Error(), data)
+// 			}
+
+// 			if r.State == FOLLOWER {
+// 				if r.Term < voteResp.VoterTerm {
+// 					r.Term = voteResp.VoterTerm
+// 					r.State = FOLLOWER
+// 					r.VotedFor = -1
+// 					// TODO ::
+// 					// cancel election timer
+// 				}
+// 			} else if r.State == CANDIDATE {
+// 				if r.Term == voteResp.VoterTerm && voteResp.Granted {
+// 					r.VotesReceived[voteResp.VoterId] = true
+// 					if r.countVotes() > int(math.Ceil((float64(NUMNODES)+1)/2)) {
+// 						r.State = LEADER
+// 						r.CurrLeaderId = r.nId
+// 						// TODO ::
+// 						// cancel election timer
+// 						// start REPLICATELOG
+// 					}
+// 				} else if r.Term < voteResp.VoterTerm {
+// 					r.Term = voteResp.VoterTerm
+// 					r.State = FOLLOWER
+// 					r.VotedFor = -1
+// 					// TODO ::
+// 					// cancel election timer
+// 				}
+// 			} else { // LEADER
+// 				if r.Term < voteResp.VoterTerm {
+// 					r.Term = voteResp.VoterTerm
+// 					r.State = FOLLOWER
+// 					r.VotedFor = -1
+// 					// TODO ::
+// 					// cancel election timer
+// 				}
+// 			}
+// 		case data := <-r.logRequestCh:
+// 			logReq := rpc.LogRequestMsg{}
+// 			err := logReq.Unmarshal(data.Data)
+// 			if err != nil {
+// 				lo.RaftError(r.nId, err.Error(), data)
+// 			}
+
+// 			if r.State == FOLLOWER {
+
+// 			} else if r.State == CANDIDATE {
+
+// 			} else { // LEADER
+
+// 			}
+// 		case data := <-r.logResponseCh:
+// 			logResp := rpc.LogResponseMsg{}
+// 			err := logResp.Unmarshal(data.Data)
+// 			if err != nil {
+// 				lo.RaftError(r.nId, err.Error(), data)
+// 			}
+
+// 			if r.State == FOLLOWER {
+
+// 			} else if r.State == CANDIDATE {
+
+// 			} else { // LEADER
+
+// 			}
+// 		}
+// 	}
+// }
+
+func (r *RaftNode) NodeMain(ctx context.Context) {
+	for {
+		c, _ := context.WithCancel(ctx)
+		if r.State == FOLLOWER {
+			r.Handle_Follower(c)
+		} else if r.State == CANDIDATE {
+			r.Handle_Candidate(c)
+		} else {
+			r.Handle_Leader(c)
+		}
+	}
+}
+
+func (n *RaftNode) resetAsFollower(ct int32) error {
+	if ct > n.Term {
+		n.Term = ct
+		n.State = FOLLOWER
+		n.VotedFor = -1
+		return errors.New("Podotyag")
+	}
+
+	return nil
+}
+
+func (n *RaftNode) Handle_Follower(ctx context.Context) {
+	tv := n.electionMinTimeout.Milliseconds()
+	tv += rand.Int63n(n.electionMaxTimeout.Milliseconds() - n.electionMinTimeout.Milliseconds())
+	timeout, cancel := context.WithTimeout(ctx, time.Duration(tv*int64(time.Millisecond)))
+	defer cancel()
+
+	select {
+	case <-ctx.Done():
+		return
+	case <-timeout.Done():
+		n.State = CANDIDATE
+		return
+	case data := <-n.voteRequestCh:
+		voteReq := rpc.VoteRequestMsg{}
+		err := voteReq.Unmarshal(data.Data)
+		if err != nil {
+			lo.RaftError(n.nId, err.Error(), data)
+			return
+		}
+		if errr := n.resetAsFollower(voteReq.CandidateTerm); errr != nil {
+			return
+		}
+
+		var lastTerm int32 = 0
+		if n.Log.Length > 0 {
+			lastTerm = n.Log.LastTerm
+		}
+
+		// if requester log is okay to vote for
+		logOk := (voteReq.CandidateLogTerm > lastTerm) ||
+			((voteReq.CandidateTerm == lastTerm) &&
+				(int(voteReq.CandidateLogLen) >= n.Log.Length))
+
+		if (voteReq.CandidateTerm == n.Term) && logOk &&
+			(n.VotedFor == voteReq.CandidateId || n.VotedFor == -1) {
+			n.VotedFor = voteReq.CandidateId
+
+			// send VoteResponse with true granted
+			resp := rpc.VoteResponseMsg{
+				VoterId:   n.nId,
+				VoterTerm: n.Term,
+				Granted:   true,
+			}
+			send_data, _ := resp.Marshal()
+			n.n.Send(voteReq.CandidateId, n.voteResponseQId, send_data)
+		} else {
+			// send VoteResponse to cadidateId with false granted
+			resp := rpc.VoteResponseMsg{
+				VoterId:   n.nId,
+				VoterTerm: n.Term,
+				Granted:   false,
+			}
+			send_data, _ := resp.Marshal()
+			n.n.Send(voteReq.CandidateId, n.voteResponseQId, send_data)
+		}
+	case data := <-n.voteResponseCh:
+		voteResp := rpc.VoteResponseMsg{}
+		err := voteResp.Unmarshal(data.Data)
+		if err != nil {
+			lo.RaftError(n.nId, err.Error(), data)
+			return
+		}
+		if errr := n.resetAsFollower(voteResp.VoterTerm); errr != nil {
+			return
+		}
+		// Ignore
+	case data := <-n.logRequestCh:
+		logReq := rpc.LogRequestMsg{}
+		err := logReq.Unmarshal(data.Data)
+		if err != nil {
+			lo.RaftError(n.nId, err.Error(), data)
+			return
+		}
+		n.resetAsFollower(logReq.LeaderTerm)
+
+		if n.Term == logReq.LeaderTerm {
+			n.CurrLeaderId = logReq.LeaderId
+		}
+
+		logOk := (n.Log.Length >= int(logReq.PrefixLen)) &&
+			((logReq.PrefixLen == 0) ||
+				(n.Log.LogArray[logReq.PrefixLen-1].Term == logReq.PrefixTerm))
+
+		if (logReq.LeaderTerm == n.Term) && logOk {
+			n.Log.AppendEntries(logReq.PrefixLen, logReq.CommitLen, logReq.Suffix)
+			ack := int(logReq.PrefixLen) + len(logReq.Suffix)
+
+			resp := rpc.LogResponseMsg{
+				FollowerId:       n.nId,
+				FollowerTerm:     n.Term,
+				LogCommitAck:     int32(ack),
+				LogCommitSuccess: true,
+			}
+			send_data, _ := resp.Marshal()
+
+			n.n.Send(n.nId, n.logResponseQId, send_data)
+		} else {
+			resp := rpc.LogResponseMsg{
+				FollowerId:       n.nId,
+				FollowerTerm:     n.Term,
+				LogCommitAck:     int32(0),
+				LogCommitSuccess: false,
+			}
+			send_data, _ := resp.Marshal()
+
+			n.n.Send(n.nId, n.logResponseQId, send_data)
+		}
+
+	case data := <-n.logResponseCh:
+		logResp := rpc.LogResponseMsg{}
+		err := logResp.Unmarshal(data.Data)
+		if err != nil {
+			lo.RaftError(n.nId, err.Error(), data)
+			return
+		}
+		if errr := n.resetAsFollower(logResp.FollowerTerm); errr != nil {
+			return
+		}
+		// Ignore
+
+	}
 }
 
 // function a node calls after a node receives VoteRequestMsg
